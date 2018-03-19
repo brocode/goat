f8dbf78cc5b3a1fc51b9df84b4c1f506de92e865
Patrick Haun <bomgar85@googlemail.com>
2018-03-19 20:27:26 +0100

    [build] use travis for releases

--- package.sh
+++ package.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-set -x -e -o pipefail
+
+set -e -u -o pipefail
+
+name=goat
 
 rm -rf target
-docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder:1.24.0 cargo build --release
-name=goat
+if [ "$TRAVIS" = true ]; then
+    chmod -R 777 .
+fi
+
+docker run -e LIBZ_SYS_STATIC=1 --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder:1.24.0 cargo build --release
+
 target_dir="target/x86_64-unknown-linux-musl/release"
 
 version=$($target_dir/$name --version | cut -d ' ' -f 2)
 
-docker run -v $(readlink -e ./$target_dir):/release -it --rm  alanfranz/fpm-within-docker:centos-7     fpm -s dir -t rpm -n $name -p /release/$name.rpm -v $version /release/$name=/usr/bin/$name
-docker run -v $(readlink -e ./$target_dir):/release -it --rm  alanfranz/fpm-within-docker:ubuntu-zesty fpm -s dir -t deb -n $name -p /release/$name.deb -v $version /release/$name=/usr/bin/$name
+docker run -v $(readlink -e ./$target_dir):/release -it --rm  alanfranz/fpm-within-docker:centos-7      fpm -s dir -t rpm -n $name -p /release/$name.rpm -v $version /release/$name=/usr/bin/$name
+docker run -v $(readlink -e ./$target_dir):/release -it --rm  alanfranz/fpm-within-docker:ubuntu-zesty  fpm -s dir -t deb -n $name -p /release/$name.deb -v $version /release/$name=/usr/bin/$name
 
