8aa983586b8e5c09ff2a6a37702f761d506c99a7
Patrick Haun <patrick@bomgar.email>
2022-10-13 15:16:05 +0200

    [chore] bump clap

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate tui;
 
 use crate::keymapping::parse_mappings;
 use crate::ui::{run, AppState};
-use clap::{crate_version, Command, Arg};
+use clap::{crate_version, Command, Arg, ArgAction, value_parser};
 use std::io;
 use std::time::Duration;
 use termion::raw::IntoRawMode;
@@ -13,7 +13,7 @@ use tui::Terminal;
 mod keymapping;
 mod ui;
 
-fn app<'a>() -> Command<'a> {
+fn app() -> Command {
   Command::new("goat")
     .version(crate_version!())
     .author("Brocode <bros@brocode.sh>")
@@ -22,16 +22,16 @@ fn app<'a>() -> Command<'a> {
       Arg::new("time")
         .short('t')
         .long("time")
-        .takes_value(true)
-        .number_of_values(1)
+        .num_args(1)
+        .value_parser(value_parser!(u32))
         .help("timer in seconds")
         .required(true),
     )
     .arg(
       Arg::new("title")
         .long("title")
-        .takes_value(true)
-        .number_of_values(1)
+        .default_value("GOAT")
+        .num_args(1)
         .help("title")
         .required(false),
     )
@@ -39,9 +39,8 @@ fn app<'a>() -> Command<'a> {
       Arg::new("mappings")
         .short('m')
         .long("mapping")
-        .takes_value(true)
-        .multiple_occurrences(true)
-        .number_of_values(1)
+        .action(ArgAction::Append)
+        .num_args(1)
         .help("Keybinding mapping. Format: <retcode>:<key>:<label> (64 <= retcode <= 113)")
         .required(false),
     )
@@ -49,13 +48,16 @@ fn app<'a>() -> Command<'a> {
 
 fn main() {
   let matches = app().get_matches();
-  let time: i32 = matches
-    .value_of("time")
-    .expect("clap should ensure this")
-    .parse::<i32>()
-    .expect("Expected time to be a valid number");
-  let title: String = matches.value_of("title").unwrap_or("GOAT").to_string();
-  let raw_mappings = matches.values_of_lossy("mappings").unwrap_or_default();
+  let time: u32 = matches
+    .get_one::<u32>("time")
+    .expect("Expected time to be a valid number")
+    .to_owned();
+  let title: String = matches.get_one::<String>("title").expect("Clap ensures this").to_owned();
+  let raw_mappings = matches.get_many::<String>("mappings")
+    .unwrap_or_default()
+    .into_iter()
+    .map(ToOwned::to_owned)
+    .collect();
   if !atty::is(atty::Stream::Stdin) || !atty::is(atty::Stream::Stdout) {
     println!("goat - sleeping for {} seconds: '{}'", time, title);
     std::thread::sleep(Duration::from_secs(time as u64));
